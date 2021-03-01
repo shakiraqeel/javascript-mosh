7a355c75eb66557cc1954c98d9c7e79919b7c921
shakir <shakiraqeel92@gmail.com>
2021-03-01 12:17:09 +0500

    updated files

---
+++ src/helloistan.js
@@ -0,0 +1,2 @@
+const resolver = function (req, res) { res.send("dosra controller") };
+module.exports = resolver;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log("Hello");
+const express = require ("express");
+const resolver = require("./helloistan");
+
+const app = express ();
+
+app.get("/", resolver);
+
+app.post("/post", (req, res) => {
+    res.send("hello browser");
+})
+
+app.listen(3566, () => console.log("Hello Server"));
+
+app.get("/s", (req, res) => {
+    res.send("hello browser");
+});
